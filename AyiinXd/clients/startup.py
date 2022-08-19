3c7270ef074524a80508056c9eaba54c6fd0a1d8
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-20 04:09:27 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -23,8 +23,8 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nMael-UserBot v{}, Copyright © 2021-2022 Mael• <https://github.com/Maelnya>"
-MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nMael-UserBot v{}, Copyright © 2021-2022 Mael• <https://github.com/Maelnya>"
+EOL = "EOL\nMael-UserBot v{}, Copyright © 2021-2022 Mael"
+MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU"
 
 async def ayiin_client(client):
     client.me = await client.get_me()
